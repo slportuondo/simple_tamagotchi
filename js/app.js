72fd87e041b48d876414522673152c9e1d10b756
Sebastian Portuondo <slportuondo@gmail.com>
2019-07-03 14:44:33 -0500

    Created game object with clock

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 console.log('hello world');
+
+const game = {
+  clock: [0, 0, 0],
+  timer: null,
+  time: 0,
+  // displayClock(){
+  //     console.log(this.clock[0], this.clock[1], this.clock[2]);
+  //     this.updateClock();
+  // },
+  updateClock() {
+    const time = new Date();
+    this.clock[0] = time.getHours();
+    this.clock[1] = time.getMinutes();
+    this.clock[2] = time.getSeconds();
+    console.log(this.clock[0], this.clock[1], this.clock[2]);
+  }
+}
+setInterval(game.updateClock(), 1000);
+// setInterval(()=> console.log('hello'), 1000)
--- js/tamaClass.js
+++ js/tamaClass.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+class Tamagotchi {
+  constructor() {
+    this.hunger = 1;
+    this.sleepiness = 1;
+    this.boredom = 1;
+  }
+  eat(){
+
+  }
+  sleep(){
+
+  }
+  play(){
+
+  }
+}
