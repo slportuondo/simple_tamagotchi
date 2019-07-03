43ad9200d4c40900226165dc9a87434419e1a361
Sebastian Portuondo <slportuondo@gmail.com>
2019-07-03 14:48:00 -0500

    added event listeners

--- js/app.js
+++ js/app.js
@@ -4,10 +4,7 @@ const game = {
   clock: [0, 0, 0],
   timer: null,
   time: 0,
-  // displayClock(){
-  //     console.log(this.clock[0], this.clock[1], this.clock[2]);
-  //     this.updateClock();
-  // },
+  tama: null,
   updateClock() {
     const time = new Date();
     this.clock[0] = time.getHours();
@@ -18,3 +15,19 @@ const game = {
 }
 setInterval(game.updateClock(), 1000);
 // setInterval(()=> console.log('hello'), 1000)
+
+
+
+
+
+
+
+
+
+
+
+
+// Event Listeners
+$('.but1').on('click', game.tama.eat());
+$('.but2').on('click', game.tama.play());
+$('.but3').on('click', game.tama.sleep());
