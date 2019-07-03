b94af112e8685cedcf2dd14cde7430dfe5546b31
Sebastian Portuondo <slportuondo@gmail.com>
2019-07-03 17:14:06 -0500

    Created tama's initial abilities

--- js/tamaClass.js
+++ js/tamaClass.js
@@ -5,12 +5,20 @@ class Tamagotchi {
     this.boredom = 1;
   }
   eat(){
-
+    if (this.hunger >= 3) {
+      this.hunger -= 2;
+    } else {
+      this.hunger = 1;
+    }
   }
   sleep(){
-
+    this.sleepiness = 1;
   }
   play(){
-
+    if (this.boredom >= 2) {
+      this.boredom -= 1;
+    } else {
+      this.boredom = 1;
+    }
   }
 }
