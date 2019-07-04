3bf1ac98d46ad8d57c838674374bc8f5ca546d08
Sebastian Portuondo <slportuondo@gmail.com>
2019-07-03 21:32:34 -0500

    Updated clock mechanics

--- js/app.js
+++ js/app.js
@@ -3,30 +3,56 @@ console.log('hello world');
 const game = {
   clock: [0, 0, 0],
   tama: null,
-  time: new Date(),
-  updateClock() {
-    // const time = new Date();
-    this.clock[0] = this.time.getHours();
-    this.clock[1] = this.time.getMinutes();
-    this.clock[2] = this.time.getSeconds();
-    console.log(this.clock[0], this.clock[1], this.clock[2]);
+  start() {
+    // prompt user for name
+    //instantiate and store
+    //and start clock
+  },
+  moreBored() {
+
+  },
+  moreTired() {
+
+  },
+  moreHungry() {
+
+  },
+  tamaDeath() {
+
+  }
+  startClock() {
+    setInterval(() => {
+      // if checks -- increase stuff
+      console.log(`The time is ${this.clock[0]}: ${this.clock[1]}: ${this.clock[2]}`);
+
+      this.clock[2]++;
+
+      if (this.clock[2] % 60 === 0) {
+        this.clock[2] = 0;
+        this.clock[1]++;
+          if (this.clock[1] % 60 === 0) {
+            this.clock[1] = 0;
+            this.clock[0]++;
+            if (this.clock[0] % 24 === 0) {
+              this.clock = [0, 0, 0];
+            }
+          }
+      }
+    }, 1000);
   }
 }
-setInterval(game.updateClock(), 1000);
-// setInterval(()=> console.log('hello'), 1000)
+game.startClock();
 
 
 
-
-
-
-
-
-
+// click any button starts game -- game.start()
 
 //
 //
-// // Event Listeners
-// $('.but1').on('click', game.tama.eat());
-// $('.but2').on('click', game.tama.play());
-// $('.but3').on('click', game.tama.sleep());
+// Event Listeners
+// $('.but1').on('click', game.tama.eat);
+$('.but1').on('click', () => {
+  game.tama.eat()
+});
+// $('.but2').on('click', game.tama.play);
+// $('.but3').on('click', game.tama.sleep);
--- js/tamaClass.js
+++ js/tamaClass.js
@@ -1,5 +1,6 @@
 class Tamagotchi {
-  constructor() {
+  constructor(name) {
+    this.name = name;
     this.hunger = 1;
     this.sleepiness = 1;
     this.boredom = 1;
