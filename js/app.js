bad83666422bc9bf4dc6f7a4b354f680981f2bf2
Sebastian Portuondo <slportuondo@gmail.com>
2019-07-03 22:39:37 -0500

    made it possible for tama to die when its health is too poor

--- js/app.js
+++ js/app.js
@@ -3,23 +3,52 @@ console.log('hello world');
 const game = {
   clock: [0, 0, 0],
   tama: null,
-  start() {
+  newGame() {
     // prompt user for name
     //instantiate and store
     //and start clock
   },
-  moreBored() {
-
-  },
-  moreTired() {
-
+  getName() {
+    //ask for userInput
+    //Return name
   },
   moreHungry() {
-
+    let howHungry = this.tama.hungry;
+    if (howHungry < 10) {
+      howHungry++;
+    }
+    if (howHungry >= 10) {
+      this.tamaDeath();
+    }
+  },
+  moreTired() {
+    let howTired = this.tama.sleepiness;
+    if (howTired < 10) {
+      howTired++;
+    }
+    if (howTired >= 10) {
+      this.tamaDeath();
+    }
+  },
+  moreBored() {
+    let howBored = this.tama.hungry;
+    if (howBored < 10) {
+      howBored += 2;
+    }
+    if (howBored >= 10) {
+      this.tamaDeath();
+    }
   },
   tamaDeath() {
-
-  }
+    let beginAgain = prompt('Your tama has died because you are a terrible parent. Begin again? Y/N');
+    if (beginAgain === "Y" || beginAgain === "y") {
+      this.tama = new Tamagotchi(getname());
+      this.clock = [0, 0, 0];
+      this.newGame();
+    } else {
+      alert("Don't have children!");
+    }
+  },
   startClock() {
     setInterval(() => {
       // if checks -- increase stuff
@@ -41,7 +70,6 @@ const game = {
     }, 1000);
   }
 }
-game.startClock();
 
 
 
@@ -54,5 +82,9 @@ game.startClock();
 $('.but1').on('click', () => {
   game.tama.eat()
 });
-// $('.but2').on('click', game.tama.play);
-// $('.but3').on('click', game.tama.sleep);
+$('.but2').on('click', () => {
+  game.tama.play();
+});
+$('.but3').on('click', () => {
+  game.tama.sleep
+});
